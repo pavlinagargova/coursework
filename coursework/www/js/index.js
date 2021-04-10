beff90c29c10b0a932d0c79131dc2e541fea4a3a
Pavlina Gargova <pavlina@luaugroup.com>
2021-04-10 22:22:06 +0300

    Config update. Plugins commit

--- coursework/www/js/index.js
+++ coursework/www/js/index.js
@@ -5,4 +5,19 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     // document.getElementById('deviceready').classList.add('ready');
+    getAppVersionNumber();
+    getConfigAppName();
+
+}
+
+function getAppVersionNumber() {
+    cordova.getAppVersion.getVersionNumber(function (version) {
+        $('#appVersion').text(version);
+    });
+}
+function getConfigAppName() {
+    cordova.getAppVersion.getAppDescription(function (name) {
+        console.log(name)
+        $('#appDescription').text(name);
+    });
 }
