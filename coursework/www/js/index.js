612290e1f960e135fbfe8782a0968580f8774a80
Pavlina Gargova <pavlina@luaugroup.com>
2021-04-16 11:50:53 +0300

    Failed attempt to set location on map

--- coursework/www/js/index.js
+++ coursework/www/js/index.js
@@ -19,32 +19,66 @@ function onDeviceReady() {
     var map = plugin.google.maps.Map.getMap(div);
 
     // If you click the button, do something...
-    var button = document.getElementById("button");
+    var button = document.getElementById("addPlace");
     button.addEventListener("click", function() {
-
-        // Move to the position with animation
-        map.animateCamera({
-            target: {lat: 37.422359, lng: -122.084344},
-            zoom: 17,
-            tilt: 60,
-            bearing: 140,
-            duration: 5000
-        });
-
-        // Add a maker
-        var marker = map.addMarker({
-            position: {lat: 37.422359, lng: -122.084344},
-            title: "Welecome to \n" +
-                "Cordova GoogleMaps plugin for iOS and Android",
-            snippet: "This plugin is awesome!",
-            animation: plugin.google.maps.Animation.BOUNCE
-        });
-
-        // Show the info window
-        marker.showInfoWindow();
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        //
+        // // Move to the position with animation
+        // map.animateCamera({
+        //     target: {lat: 37.422359, lng: -122.084344},
+        //     zoom: 17,
+        //     tilt: 60,
+        //     bearing: 140,
+        //     duration: 5000
+        // });
+        //
+        // // Add a maker
+        // var marker = map.addMarker({
+        //     position: {lat: 37.422359, lng: -122.084344},
+        //     title: "Welecome to \n" +
+        //         "Cordova GoogleMaps plugin for iOS and Android",
+        //     snippet: "This plugin is awesome!",
+        //     animation: plugin.google.maps.Animation.BOUNCE
+        // });
+        //
+        // // Show the info window
+        // marker.showInfoWindow();
 
     });
 
+}
+
+function onSuccess(position) {
+    var div = document.getElementById("map_canvas");
+    console.log(1)
+
+    var map = plugin.google.maps.Map.getMap(div);
+    var lat = position.coords.latitude;
+    var lang = position.coords.longitude;
+    // Move to the position with animation
+    map.animateCamera({
+        target: {lat: lat, lng: lang},
+        zoom: 17,
+        tilt: 60,
+        bearing: 140,
+        duration: 5000
+    });
+
+    // Add a maker
+    var marker = map.addMarker({
+        position: {lat: lat, lng: lang},
+        title: "Welecome to \n" +
+            "Cordova GoogleMaps plugin for iOS and Android",
+        snippet: "This plugin is awesome!",
+        animation: plugin.google.maps.Animation.BOUNCE
+    });
+
+    // Show the info window
+    marker.showInfoWindow();
+}
+function onError(error) {
+    alert('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n');
 }
 
 function getAppVersionNumber() {
